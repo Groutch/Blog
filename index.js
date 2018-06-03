24eec0fc98d6e03572cc908c9146d5224728fb35
Groutch <hurizen1@hotmail.com>
2018-06-03 12:42:49 +0200

    affichage articles depuis json

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 $(document).ready(function () {
     //Si il y a un utilsateur loggé, on l'affiche dans le message de bienvenue
+    //on cache le bouton pour se login/affiche le bouton pour se logout
     if (localStorage.getItem("userlogged") != null) {
-
         $(".text-welcome").empty();
         $(".text-welcome").append("Bienvenue " + localStorage.getItem("userlogged"));
+        $(".login").hide();
+        $(".signin").hide();
+        $(".logout").show();
+    }else{
+        $(".login").show();
+        $(".signin").show();
+        $(".logout").hide();
     }
     //Si on clique sur se déco, on se déco
+    //on affiche le bouton pour se login/cache le bouton pour se logout
     $("#logout").on("click", logout);
 
     function logout() {
         if (localStorage.getItem("userlogged") != null) {
             localStorage.removeItem("userlogged");
-            alert("Utilisateur déconnecté")
+            alert("Utilisateur déconnecté");
         } else {
             alert("Pas connecté");
         }
     }
     // on charge la liste des billets
-    
+    $.getJSON('billets.json', function (artlist) {
+        $('.artlist').empty;
+        for (var i = 0; i < artlist.billets.length; i++) {
+            $('.artlist').append(`<div class="billet" id="art${i}">
+    <h2>${artlist.billets[i].title}</h2>
+    ${artlist.billets[i].body}
+    <footer class="text-right">
+        ${artlist.billets[i].author} - ${artlist.billets[i].date}
+    </footer>
+</div>`);
+        }
+
+    });
 });
